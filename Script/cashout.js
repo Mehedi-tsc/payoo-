519795755454f75241208bc99b18e0a7073dd3d5
Mehedi-tsc <mehedimmc1@gmail.com>
2026-02-19 06:51:34 +0600

    second push

--- Script/cashout.js
+++ Script/cashout.js
@@ -1,1 +1,29 @@
-console.log('Mehedi')
+document.getElementById('login-btn')
+    .addEventListener('click',function(){
+    const cashoutNumberInput=document.getElementById('cashout-number');
+    const cashoutNumber = cashoutNumberInput.value;
+    if(cashoutNumber.length!=11){
+        alert('Invalid Agent Number')
+        return;
+    }
+    
+    const cashoutAmountInput=document.getElementById('cashout-amount');
+    const cashoutAmount = cashoutAmountInput.value;
+    const balanceElement= document.getElementById('balance');
+    const balance = balanceElement.innerText;
+    const newBalance= Number(balance)-Number(cashoutAmount);
+    if(newBalance<0){
+        alert('Invalid Amount');
+        return;
+    }
+    const pinInput=document.getElementById('cashout-pin');
+    const pin=pinInput.value;
+    if(pin==='1234'){
+        alert('cashout successfull')
+        balanceElement.innerText=newBalance;
+    }
+    else{
+        alert('Wrong Pin')
+    }
+    
+})
