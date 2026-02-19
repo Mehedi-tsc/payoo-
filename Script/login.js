2502af6b9b3608e0b34c9105c9a61725dfdb4e03
Mehedi-tsc <mehedimmc1@gmail.com>
2026-02-19 06:09:38 +0600

    first commit

---
+++ Script/login.js
@@ -0,0 +1,15 @@
+document.getElementById('login-btn')
+    .addEventListener('click',function(){
+        const inputNumber = document.getElementById('input-number')
+        const mobile = inputNumber.value;
+        const inputPin = document.getElementById('input-pin');
+        const pin = inputPin.value;
+        if(mobile==='01734544516'&& pin==='1234'){
+            alert('login succesfully')
+            window.location.assign("/home.html")
+        }
+        else{
+            alert('Mobile or Pin is not correct')
+            return;
+        }
+    })
